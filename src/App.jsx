c0dc21eeb1e0ac3304494a2baa4a0220f5bfed2a
smartful <remi_rodrigues@hotmail.com>
2025-08-17 22:18:46 +0200

    create About page and a Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router';
+import AboutPage from './pages/About';
 import HomePage from './pages/Home';
+import Header from './components/Header';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
@@ -33,24 +35,28 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 };
 
